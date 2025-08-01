1b0a660d22992c050c543011e4991e388e2b137b
unknown <miqueiassantos1552@gmail.com>
2025-07-31 22:44:28 -0300

    animacoes scroll

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  @ViewChild('servicos')
+  areaServicos!: ElementRef; 
+  descer(){
+    this.areaServicos.nativeElement.scrollIntoView({behavior: 'smooth' })
+  }
 
 }
